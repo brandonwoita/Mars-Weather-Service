e5b935af2ae1ebc0bcbfd590b8e0740d70286a1a
brandonwoita <Brandon@Brandons-MacBook-Pro.local>
2020-03-31 17:32:32 -0500

    updated api key

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const API_KEY = 'DEMO_KEY'
+const API_KEY = 'BIGmDq8x1soqabd0hlhjm1LLqcKPSNEe9XNjNKqx'
 const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
 
 const previousWeatherToggle = document.querySelector('.show-previous-weather');
